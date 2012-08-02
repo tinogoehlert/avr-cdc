71cdbaf638f97b8ccfc518259639c01c9e5f04d0
Tino Goehlert <t.goehlert@googlemail.com>
2012-08-02 21:22:39 +0200

    DTR for attiny2313@12MHZ

---
+++ cdc232.2011-06-24/tiny2313/default/dep/usbdrv.o.d
@@ -0,0 +1,49 @@
+usbdrv.o: ../../usbdrv/usbdrv.c ../../usbdrv/usbportability.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/io.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/sfr_defs.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/inttypes.h \
+ /usr/lib/gcc/avr/4.7.1/include/stdint.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/stdint.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/iotn2313.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/portpins.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/common.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/version.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/fuse.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/lock.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/pgmspace.h \
+ /usr/lib/gcc/avr/4.7.1/include/stddef.h ../../usbdrv/usbdrv.h \
+ ../usbconfig.h ../../usbdrv/oddebug.h
+
+../../usbdrv/usbportability.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/io.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/sfr_defs.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/4.7.1/include/stdint.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/stdint.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/iotn2313.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/portpins.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/common.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/version.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/fuse.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/lock.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/pgmspace.h:
+
+/usr/lib/gcc/avr/4.7.1/include/stddef.h:
+
+../../usbdrv/usbdrv.h:
+
+../usbconfig.h:
+
+../../usbdrv/oddebug.h:
---
+++ cdc232.2011-06-24/tiny2313/default/dep/usbdrvasm.o.d
@@ -0,0 +1,37 @@
+usbdrvasm.o: ../../usbdrv/usbdrvasm.S ../../usbdrv/usbportability.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/io.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/sfr_defs.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/iotn2313.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/portpins.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/common.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/version.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/fuse.h \
+ /usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/lock.h \
+ ../../usbdrv/usbdrv.h ../usbconfig.h ../../usbdrv/usbdrvasm12.inc \
+ ../../usbdrv/asmcommon.inc
+
+../../usbdrv/usbportability.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/io.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/sfr_defs.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/iotn2313.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/portpins.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/common.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/version.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/fuse.h:
+
+/usr/lib/gcc/avr/4.7.1/../../../../avr/include/avr/lock.h:
+
+../../usbdrv/usbdrv.h:
+
+../usbconfig.h:
+
+../../usbdrv/usbdrvasm12.inc:
+
+../../usbdrv/asmcommon.inc:
